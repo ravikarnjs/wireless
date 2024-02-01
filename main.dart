1639b8b6c07ca7618576bc783f4dddb15d539e20
ravikarnjs <142828860+ravikarnjs@users.noreply.github.com>
2024-02-01 12:52:38 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,118 +1,101 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
-main() => runApp(App());
+void main() => runApp(MyApp());
 
-class App extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: TextDirection.ltr,
-      child: Container(
-        padding: EdgeInsets.symmetric(
-          vertical: 60.0,
-          horizontal: 80.0,
-        ),
-        color: Color(0xFFFFFFFF),
-        child: Content(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MyHomePage(title: 'Product layout demo home page'),
     );
   }
 }
 
-class Content extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Counter('6488210'),
-        Counter('Ravikarn Jarungjitvittawas'),
-        Counter('HomeAlert'),
-        Counter('6488120 6488179 6488181 6488210'),
-      ],
-    );
+    return Scaffold(
+        appBar: AppBar(title: Text("Product Listing")),
+        body: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+          children: <Widget>[
+            ProductBox(
+                name: "iPhone",
+                description: "iPhone is the stylist phone ever",
+                price: 1000,
+                image: "iphone.png"),
+            ProductBox(
+                name: "Pixel",
+                description: "Pixel is the most featureful phone ever",
+                price: 800,
+                image: "pixel.png"),
+            ProductBox(
+                name: "Laptop",
+                description: "Laptop is most productive development tool",
+                price: 2000,
+                image: "laptop.png"),
+            ProductBox(
+                name: "Tablet",
+                description:
+                    "Tablet is the most useful device ever for meeting",
+                price: 1500,
+                image: "tablet.png"),
+            ProductBox(
+                name: "Pendrive",
+                description: "Pendrive is useful storage medium",
+                price: 100,
+                image: "pendrive.png"),
+            ProductBox(
+                name: "Floppy Drive",
+                description: "Floppy drive is useful rescue storage medium",
+                price: 20,
+                image: "floppy.png"),
+          ],
+        ));
   }
 }
 
-class Counter extends StatefulWidget {
-  final String _name;
-  Counter(this._name);
-
-  @override
-  State<Counter> createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  int count = 0;
-
-  @override
+class ProductBox extends StatelessWidget {
+  ProductBox(
+      {Key? key,
+      required this.name,
+      required this.description,
+      required this.price,
+      required this.image})
+      : super(key: key);
+  final String name;
+  final String description;
+  final int price;
+  final String image;
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(bottom: 10.0),
-      padding: EdgeInsets.all(4.0),
-      decoration: BoxDecoration(
-        border: Border.all(color: Color.fromARGB(255, 98, 2, 253)),
-        borderRadius: BorderRadius.circular(4.0),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // [widget] is the property of the State class that stores
-          // the instance of the [StatefulWidget] ([Counter] in our case)
-          _CounterLabel(widget._name),
-          _CounterButton(
-            count,
-            onPressed: () {
-              setState(() {
-                ++count;
-              });
-            },
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class _CounterLabel extends StatelessWidget {
-  static const textStyle = TextStyle(
-    color: Color(0xFF000000),
-    fontSize: 26.0,
-  );
-
-  final String _label;
-  _CounterLabel(this._label);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      _label,
-      style: _CounterLabel.textStyle,
-    );
-  }
-}
-
-class _CounterButton extends StatelessWidget {
-  final count;
-  final onPressed;
-  _CounterButton(this.count, {@required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 6.0),
-        decoration: BoxDecoration(
-          color: Color.fromARGB(255, 98, 2, 253),
-          borderRadius: BorderRadius.circular(4.0),
-        ),
-        child: Center(
-          child: Text(
-            '$count',
-            style: TextStyle(fontSize: 20.0),
-          ),
-        ),
-      ),
-    );
+        padding: EdgeInsets.all(2),
+        height: 120,
+        child: Card(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+              Image.asset("assets/img/" + image),
+              Expanded(
+                  child: Container(
+                      padding: EdgeInsets.all(5),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Text(this.name,
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text(this.description),
+                          Text("Price: " + this.price.toString()),
+                        ],
+                      )))
+            ])));
   }
 }
